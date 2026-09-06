3951ec74c9f2383b937a8c7352e2ba5af7ec78bc
Rajat <chxsexo@gmail.com>
2026-09-06 08:13:55 +0530

    Add Coral theme colors in Color.kt

---
+++ app/src/main/java/com/rajatxo/coral/ui/theme/Color.kt
@@ -0,0 +1,10 @@
+package com.rajatxo.coral.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Coral Theme Colors
+val CoralBackground = Color(0xFF0A0A0A)
+val CoralSurface = Color(0xFF1A1A1A)
+val CoralAccent = Color(0xFFFF6B6B) // Coral pink/red
+val CoralTextPrimary = Color(0xFFFFFFFF)
+val CoralTextSecondary = Color(0xFFB0B0B0)
