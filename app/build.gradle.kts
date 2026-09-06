d227fff5c19bfb263e4e1e7b274188aa6a3bb9a3
Rajat <chxsexo@gmail.com>
2026-09-06 08:58:26 +0530

    Add kotlinx-coroutines-guava dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,4 +67,5 @@ dependencies {
     implementation("androidx.palette:palette-ktx:1.0.0")
     implementation("androidx.navigation:navigation-compose:2.8.5")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.9.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-guava:1.9.0")
 }
