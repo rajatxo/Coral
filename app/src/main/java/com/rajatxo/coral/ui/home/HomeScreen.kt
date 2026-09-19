3203ceb68afabaf3e704adf39137c62fd4f13733
Z User <z@container>
2026-09-19 18:03:19 +0000

    fix: add Brush import

--- app/src/main/java/com/rajatxo/coral/ui/home/HomeScreen.kt
+++ app/src/main/java/com/rajatxo/coral/ui/home/HomeScreen.kt
@@ -50,6 +50,7 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.drawWithContent
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.input.pointer.pointerInput
